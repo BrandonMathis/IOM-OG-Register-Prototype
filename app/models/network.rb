a2ec72c8a54ca489abc02702d437481669a30462
Doug Alcorn <dougalcorn@gmail.com>
2010-02-05 16:36:04 -0500

    proper xml builder for network element

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,12 @@
 class Network < CcomObjectWithChildren
   has_many :entry_points, :class_name => "NetworkConnection"
+
+  def build_xml(builder)
+    super(builder)
+    entry_points.each do |e|
+      builder.hasEntryPoint do |b|
+        e.build_xml(b)
+      end
+    end
+  end
 end
--- test/unit/network_test.rb
+++ test/unit/network_test.rb
@@ -8,4 +8,20 @@ class NetworkTest < ActiveSupport::TestCase
   should "support an entry point as a network connection" do
     assert_valid Factory.create(:network, :entry_points => [Factory.create(:network_connection)])
   end
+
+  context "generating xml" do
+    setup do
+      @network = Factory.create(:network, 
+                                :entry_points => [Factory.create(:network_connection)])
+      builder = Builder::XmlMarkup.new
+      xml = builder.Network do |b|
+        @network.build_xml(b)
+      end
+      @doc = Nokogiri::XML.parse(xml)
+    end
+    
+    should "have proper xml name for each entry point" do
+      assert_equal 1, @doc.xpath("//Network/hasEntryPoint").size
+    end
+  end
 end
