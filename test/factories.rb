7e5fe933b17cdc1aed08c6601f9b241b55cd68de
Doug Alcorn <dougalcorn@gmail.com>
2010-02-05 12:13:10 -0500

    factory girl parent's don't work like we thought

--- test/factories.rb
+++ test/factories.rb
@@ -1,17 +1,32 @@
 Factory.sequence(:guid) { |i| "#{Time.now.to_i}#{i}" }
 Factory.sequence(:id_in_source) { |i| "#{i}" }
 
-Factory.define(:ccom_object) do |f|
-  f.guid { Factory.next :guid }
-  f.id_in_source { Factory.next :id_in_source }
-  f.source_id "exxon.com/uehm/ramp/V1.0"
+def ccom_entity_fields(factory)
+  factory.guid { Factory.next :guid }
+  factory.id_in_source { Factory.next :id_in_source }
+  factory.source_id "exxon.com/uehm/ramp/V1.0"
 end
 
-Factory.define(:object_type, :parent => :ccom_object) do |f|
+Factory.define(:ccom_entity) do |f|
+  ccom_entity_fields(f)
+end
+
+Factory.define(:ccom_object) do |f|
+  ccom_entity_fields(f)
+end
+
+Factory.define(:object_type) do |f|
+  ccom_entity_fields(f)
   f.sequence(:user_name) { |i| "Object Type #{i}" }
 end
 
-Factory.define(:attribute_type, :parent => :ccom_object) do |f|
+Factory.define(:attribute_type) do |f|
+  ccom_entity_fields(f)
   f.sequence(:user_name) { |i| "Attribute Type #{i}" }
+  f.reference_unit_type "Foo"
 end
 
+Factory.define(:object_data) do |f|
+  ccom_entity_fields(f)
+  f.attribute_type { |a| a.association(:attribute_type) }
+end
