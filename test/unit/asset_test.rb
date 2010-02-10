3d075eefd5529fe5a4487e180195ec401cd03dfc
Doug Alcorn <dougalcorn@gmail.com>
2010-02-10 13:05:34 -0500

    improving asset's tests to be more explicit

--- test/unit/asset_test.rb
+++ test/unit/asset_test.rb
@@ -6,22 +6,34 @@ class AssetTest < ActiveSupport::TestCase
   end
 
   should "support a serial number" do
-    assert_valid Factory.create(:asset, :serial_number => "3Z84G32AA0-4")
+    sn = "3Z84G32AA0-4"
+    assert_valid asset = Factory.create(:asset, :serial_number => sn)
+    assert_equal sn, asset.serial_number
   end
 
   should "support a manufacturer" do
-    assert_valid Factory.create(:asset, :manufacturer => Factory.create(:manufacturer))
+    manufacturer = Factory.create(:manufacturer)
+    assert_valid asset = Factory.create(:asset, :manufacturer => manufacturer)
+    assert_equal manufacturer, asset.manufacturer
   end
 
   should "support a model" do
-    assert_valid Factory.create(:asset, :model => Factory.create(:model))
+    model = Factory.create(:model)
+    assert_valid asset = Factory.create(:asset, :model => model)
+    assert_equal model, asset.model
   end
 
   should "support an asset config network" do
-    assert_valid Factory.create(:asset, :asset_config_network => Factory.create(:asset_config_network))
+    asset_config_network = Factory.create(:asset_config_network)
+    assert_valid asset = Factory.create(:asset, :asset_config_network => asset_config_network)
+    assert_equal asset_config_network, asset.asset_config_network
   end
 
   should "support an installed on segment" do
-    assert_valid Factory.create(:asset, :installed_on_segment => Factory.create(:segment))
+    segment = Factory.create(:segment)
+    assert_valid asset = Factory.create(:asset)
+    asset.installed_on_segment = segment
+    assert asset.save
+    assert_equal segment, asset.installed_on_segment
   end
 end
