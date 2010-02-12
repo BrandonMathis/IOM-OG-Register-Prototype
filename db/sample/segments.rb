9acc322c4cf0b115618a77be12f85d5da4cc6f23
Doug Alcorn <dougalcorn@gmail.com>
2010-02-12 13:07:30 -0500

    creating db/sample with some ruby to generate sample objects

---
+++ db/sample/segments.rb
@@ -0,0 +1,6 @@
+s1 = Segment.new(:guid => "515b3eae-93bf-44da-a239-2436ece17deb", :source_id => "foreverinfrastructure.com/topology/V1.0", :user_tag => "CU-1", :user_name => "Crude Unit #1")
+                 :object_type => ObjectType.new(:guid => "530e49d5-392e-455d-83ec-ca47983cc990",
+                                                :id_in_source => "0000000000000000.1.202",
+                                                :user_tag => "ISA-95 Site",
+                                                :user_name => "ISA-95 Site"))
+
