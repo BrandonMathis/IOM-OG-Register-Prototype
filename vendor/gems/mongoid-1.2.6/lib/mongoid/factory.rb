2376397b8d248442056929d3f7101823e0f01640
Doug Alcorn <dougalcorn@gmail.com>
2010-02-17 17:46:36 -0500

    upgrading mongoid to 1.2.6

---
+++ vendor/gems/mongoid-1.2.6/lib/mongoid/factory.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+module Mongoid #:nodoc:
+  class Factory #:nodoc:
+    # Builds a new +Document+ from the supplied attributes.
+    #
+    # Example:
+    #
+    # <tt>Mongoid::Factory.build(Person, {})</tt>
+    #
+    # Options:
+    #
+    # klass: The class to instantiate from if _type is not present.
+    # attributes: The +Document+ attributes.
+    def self.build(klass, attrs)
+      type = attrs["_type"]
+      type ? type.constantize.instantiate(attrs) : klass.instantiate(attrs)
+    end
+  end
+end
---
+++ vendor/gems/mongoid-1.2.6/spec/unit/mongoid/config_spec.rb
@@ -0,0 +1,158 @@
+require "spec_helper"
+
+describe Mongoid::Config do
+
+  after :all do
+    config.raise_not_found_error = true
+  end
+
+  let(:config) { Mongoid::Config.instance }
+
+  describe "#database=" do
+
+    context "when object provided is not a Mongo::DB" do
+
+      it "raises an error" do
+        lambda { config.database = "Test" }.should raise_error
+      end
+
+    end
+
+  end
+
+  describe "#master=" do
+
+    context "when object provided is not a Mongo::DB" do
+
+      it "raises an error" do
+        lambda { config.master = "Test" }.should raise_error
+      end
+
+    end
+
+  end
+
+  describe "#parameterize_keys" do
+
+    it "defaults to true" do
+      config.parameterize_keys.should == true
+    end
+  end
+
+  describe "#persist_in_safe_mode=" do
+
+    context "when setting to true" do
+
+      before do
+        config.persist_in_safe_mode = true
+      end
+
+      it "sets the value" do
+        config.persist_in_safe_mode.should == true
+      end
+
+    end
+
+    context "when setting to false" do
+
+      before do
+        config.persist_in_safe_mode = false
+      end
+
+      it "sets the value" do
+        config.persist_in_safe_mode.should == false
+      end
+
+    end
+
+  end
+
+  describe "#raise_not_found_error=" do
+
+    context "when setting to true" do
+
+      before do
+        config.raise_not_found_error = true
+      end
+
+      it "sets the value" do
+        config.raise_not_found_error.should == true
+      end
+
+    end
+
+    context "when setting to false" do
+
+      before do
+        config.raise_not_found_error = false
+      end
+
+      it "sets the value" do
+        config.raise_not_found_error.should == false
+      end
+
+    end
+
+  end
+
+  describe "#reconnect_time" do
+
+    it "defaults to 3" do
+      config.reconnect_time.should == 3
+    end
+
+  end
+
+  describe "#reconnect_time=" do
+
+    after do
+      config.reconnect_time = 3
+    end
+
+    it "sets the time" do
+      config.reconnect_time = 5
+      config.reconnect_time.should == 5
+    end
+  end
+
+  describe "#slaves=" do
+
+    context "when object provided is not a Mongo::DB" do
+
+      it "raises an error" do
+        lambda { config.slaves = ["Test"] }.should raise_error
+      end
+
+    end
+
+  end
+
+  describe "#allow_dynamic_fields=" do
+
+    context "when setting to true" do
+
+      before do
+        config.allow_dynamic_fields = true
+      end
+
+      it "sets the value" do
+        config.allow_dynamic_fields.should == true
+      end
+
+    end
+
+    context "when setting to false" do
+
+      before do
+        config.allow_dynamic_fields = false
+      end
+
+      it "sets the value" do
+        config.allow_dynamic_fields.should == false
+      end
+
+    end
+
+  end
+
+end
