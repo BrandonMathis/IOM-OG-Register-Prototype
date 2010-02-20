c30f17396ce193bb6809fd77ada1241dde6ecae1
Doug Alcorn <dougalcorn@gmail.com>
2010-02-19 19:36:13 -0500

    display multiple object data per meas_location on the asset's data sheet

--- test/integration/assets_controller_test.rb
+++ test/integration/assets_controller_test.rb
@@ -4,11 +4,15 @@ class AssetsControllerTest < ActionController::IntegrationTest
 
   context "with an asset and a single segment datasheet" do
     setup do
-      @attr_type = Factory.create(:attribute_type, :user_tag => "Armature Voltage, Rated")
-      @eng_unit = Factory.create(:eng_unit_type, :user_tag => "Volts")
-      @obj_data = Factory.create(:object_datum, :data => "2300", 
-                                 :attribute_type => @attr_type, :eng_unit_type => @eng_unit)
-      @meas_loc1 = Factory.create(:meas_location, :object_data => @obj_data)
+      @attr_type1 = Factory.create(:attribute_type, :user_tag => "Armature Voltage, Rated")
+      @eng_unit1 = Factory.create(:eng_unit_type, :user_tag => "Volts")
+      @obj_data1 = Factory.create(:object_datum, :data => "2300", 
+                                 :attribute_type => @attr_type1, :eng_unit_type => @eng_unit1)
+      @attr_type2 = Factory.create(:attribute_type, :user_tag => "Armature 2")
+      @eng_unit2 = Factory.create(:eng_unit_type, :user_tag => "Volts 2")
+      @obj_data2 = Factory.create(:object_datum, :data => "00", 
+                                 :attribute_type => @attr_type2, :eng_unit_type => @eng_unit2)
+      @meas_loc1 = Factory.create(:meas_location, :object_data => [@obj_data1, @obj_data2])
       @segment = Factory.create(:segment, :user_tag => "ELEC SPEC", :meas_locations => [@meas_loc1])
       
       ep = Factory.create(:network_connection, :source => @segment)
@@ -24,15 +28,23 @@ class AssetsControllerTest < ActionController::IntegrationTest
     end
 
     should "have the meas loc's user tag" do
-      assert_select "tr[id=?] > td.user_tag", @meas_loc1.guid, @meas_loc1.user_tag
+      assert_select "tr > td.user_tag", @meas_loc1.user_tag
     end
 
     should "have the meas loc's attribute type" do
-      assert_select "tr[id=?] > td.attribute_type", @meas_loc1.guid, @attr_type.user_tag
+      assert_select "tr > td.attribute_type", @attr_type1.user_tag
     end
 
     should "have the meas loc's data value" do
-      assert_select "tr[id=?] > td.object_data", @meas_loc1.guid, "2300 Volts"
+      assert_select "tr > td.object_data", "2300 Volts"
+    end
+
+    should "have the meas loc's second attribute type" do
+      assert_select "tr > td.attribute_type", @attr_type2.user_tag
+    end
+
+    should "have the meas loc's second data value" do
+      assert_select "tr > td.object_data", "00 Volts 2"
     end
   end
 end
