e17fa716b80af6bf05660c02b554b4f3de3073c4
Doug Alcorn <dougalcorn@gmail.com>
2010-02-20 14:51:24 -0500

    move the events dir to rails root, add timestamp to postback server event filename

--- lib/postback_server.rb
+++ lib/postback_server.rb
@@ -10,14 +10,15 @@ end
 
 post '/events' do
   data = request.body.read
+  time = Time.now.strftime('%Y%m%dT%H%M%S')
   if (doc = Nokogiri::XML.parse(data) rescue nil)
     unless (user_tag = doc.xpath("//ccom:userTag", "ccom" => "http://www.mimosa.org/osa-eai/v3-3/xml/CCOM-ML").first).nil?
       # FIXME: don't have good tests for this filename escaping
       # http://www.ruby-forum.com/topic/124471
-      filename = File.basename(user_tag.content.tr("/\000", "")) + ".xml"
+      filename = time + " " + File.basename(user_tag.content.tr("/\000", "")) + ".xml"
     end
   end
-  filename ||= "No CCOMData userTag: #{Time.now.strftime('%Y%m%dT%H%M%S')}.xml"
+  filename ||= "No CCOMData userTag: #{time}.xml"
   File.open(File.join(EVENTS_PATH, filename), "w") do |f|
     f.write data
   end
