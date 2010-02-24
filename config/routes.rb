f537762e6f346da59779de5a0aa08a34f2187729
Doug Alcorn <dougalcorn@gmail.com>
2010-02-24 16:46:47 -0500

    add some navigational love

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+
+  map.root :controller => "ccom_data"
 end
