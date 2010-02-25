44dfeb587f68cd1ea18dd46b94ddd7092317d513
Doug Alcorn <dougalcorn@gmail.com>
2010-02-25 16:37:07 -0500

    adding tabs to the asset show, getting JS to work on the asset show and the segment show as well as topologies show

--- public/javascripts/application/topologies.js
+++ public/javascripts/application/topologies.js
@@ -12,25 +12,32 @@ $(function() {
         });
         return false;
     });
+    setupAssetDetails();
+    setupSegmentDetails();
 })
 
 function setupSegmentDetails() {
-    var last_tab = $("ul.tab_links li").size() - 1;
-    $("#tabs").tabs({selected: last_tab});
+    var last_tab = $("div#segment_tabs > ul.tab_links li").size() - 1;
+    $("#segment_tabs").tabs({selected: last_tab});
     $(".show_installed_asset_link").click(function() {
         var asset_title = $(this).attr("title");
         $.get($(this).attr("href"), function(data) {
             var details = $("#asset-details");
             details.html(data);
-            $("#asset-dialog").attr("title", asset_title);
-            $("#asset-dialog").dialog({ width: ($(window).width() * .6),
+            setupAssetDetails();
+            $("#asset_dialog").attr("title", asset_title);
+            $("#asset_dialog").dialog({ width: ($(window).width() * .6),
             modal: true });
         });
         return false;
     });
     $(".edit_segment").ajaxForm({
-        target: "#segment-details",
+        target: "#segment_details",
         success: setupSegmentDetails
     });
 }
 
+function setupAssetDetails() {
+    var last_tab = $("div#asset_tabs > ul.tab_links li").size() - 1;
+    $("#asset_tabs").tabs({selected: last_tab});
+}
