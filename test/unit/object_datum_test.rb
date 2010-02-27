309de95bbb38a3a50456b1ac2a50aeecb70bf91c
Doug Alcorn <dougalcorn@gmail.com>
2010-02-26 19:37:11 -0500

    cleaning up object datum test

--- test/unit/object_datum_test.rb
+++ test/unit/object_datum_test.rb
@@ -21,25 +21,20 @@ class ObjectDatumTest < ActiveSupport::TestCase
     setup do
       @datum = Factory.create(:object_datum, :eng_unit_type => Factory.create(:eng_unit_type))
 
-      builder = Builder::XmlMarkup.new
-      @xsi = "somthine"
-      @xml = builder.tag!("ObjectDatum", "xmlns:xsi" => @xsi) do |b|
-        @datum.build_xml(b)
-      end
-      @doc = Nokogiri::XML.parse(@xml)
+      @doc = Nokogiri::XML.parse(@datum.to_xml)
     end
     
     should "have proper path for data value" do
-      assert_not_nil data_node = @doc.xpath("//ObjectDatum/hasData/TextType").first
+      assert_not_nil data_node = @doc.mimosa_xpath("//ObjectDatum/hasData/TextType").first
       assert_equal @datum.data, data_node.content
     end
 
     should "have the proper path for attribute type" do
-      assert_not_nil attribute_type_node = @doc.xpath("//ObjectDatum/hasAttributeType").first
+      assert_not_nil attribute_type_node = @doc.mimosa_xpath("//ObjectDatum/hasAttributeType").first
     end
 
     should "have the proper path for eng unit type" do
-      assert_not_nil attribute_type_node = @doc.xpath("//ObjectDatum/hasEngUnitType").first
+      assert_not_nil attribute_type_node = @doc.mimosa_xpath("//ObjectDatum/hasEngUnitType").first
     end
   end
 end
