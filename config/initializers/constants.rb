64eb1407dcea254fcb0f264cc893530f03fc323a
BrandonMathis <Brandon@KeysetTS.com>
2010-08-25 16:24:11 -0400

    Modified XML default namespace constatnt in 	config/initializers/constants.rb 	lib/postback_server.rb 	test/* 	db/xml/topology.xml (used for testing)

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
-MIMOSA_XMLNS = "http://www.mimosa.org/osa-eai/v3-3/xml/CCOM-ML"
+MIMOSA_XMLNS = "http://www.mimosa.org/osa-eai/v3-2/xml/CCOM-ML"
 XSI_XMLNS = "http://www.w3.org/2001/XMLSchema-instance"
 POSTBACK_HOST = "localhost"
 POSTBACK_PORT = "4567"
