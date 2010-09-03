4cb9ad852016cbdd748a4608646975d03f9c10cf
BrandonMathis <Brandon@KeysetTS.com>
2010-09-03 12:46:12 -0400

    Added Dropdown for Valid Network and Type

--- app/controllers/ccom_controller.rb
+++ app/controllers/ccom_controller.rb
@@ -18,13 +18,21 @@ class CcomController < ApplicationController
   end
   
   def new
+    @all_networks = ValidNetwork.find(:all)
+    @all_assets = Asset.find(:all)
+    @types = []
+    @networks = []
+    @all_assets.each{ |a| @types << a.type unless @types.include?(a.type)}
+    @all_networks.each{ |n| @networks << n unless @networks.include?(n) }
     respond_to do |format|
       format.html
     end
   end
   
   def create
-    @entity = Asset.create(params[:entity])
+    passed_values = params[:entity]
+    @entity = Asset.create(passed_values)
+    @entity.update_attributes(:valid_network => ValidNetwork.find_by_guid(passed_values[:network_id]))
     
     respond_to do |format|
       if @entity.save
