98444977c4ac760ae250c18613a5673faa4a2bb9
BrandonMathis <Brandon@KeysetTS.com>
2010-09-03 15:13:35 -0400

    Added dropdown for user to select Type for Asset Creation

--- app/controllers/ccom_controller.rb
+++ app/controllers/ccom_controller.rb
@@ -17,22 +17,38 @@ class CcomController < ApplicationController
     end
   end
   
+  # Setup for the Entity Creation Form
+  # Types and Valid Networks are pulled from exsisting Assets which should
+  # have been uploaded via CCOM XML (or maybe defined by admin in future)
+  #
+  # Dup Networks and Types are ignored if they are 100% similar. The slightest
+  # difference will cause the type or network to be included in the dropdown
   def new
     @all_networks = ValidNetwork.find(:all)
     @all_assets = Asset.find(:all)
     @types = []
     @networks = []
-    @all_assets.each{ |a| @types << a.type unless @types.include?(a.type)}
+    @all_assets.each{ |a| @types << a.type unless a.type.nil? || @types.include?(a.type)}
     @all_networks.each{ |n| @networks << n unless @networks.include?(n) }
     respond_to do |format|
       format.html
     end
   end
   
+  # Create Entity for has been submitted. We run a lookup for ValidNetwork
+  # and Type based off GUID passed by the form (identifying what Type and
+  # ValidNetwork were selected)
+  #
+  # Top level elements are passed in a manner that can be used right away 
+  # by the mongoid create function
+  #
+  # User is redirectred to Assets page uppon form submission
   def create
     passed_values = params[:entity]
     @entity = Asset.create(passed_values)
-    @entity.update_attributes(:valid_network => ValidNetwork.find_by_guid(passed_values[:network_id]))
+    @entity.update_attributes(
+                    :valid_network => ValidNetwork.find_by_guid(passed_values[:network_id]),
+                    :type => Type.find_by_guid(passed_values[:type_id]))
     
     respond_to do |format|
       if @entity.save
