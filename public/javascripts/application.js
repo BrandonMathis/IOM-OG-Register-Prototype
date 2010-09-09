0a7c711a773665ac93580f6e910329dcf5755355
BrandonMathis <Brandon@KeysetTS.com>
2010-09-09 01:10:36 -0400

    added expand collapse icon for assets

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+	$(".asset-name").toggle(
+		function() {
+			$(this).siblings("img").attr("src", "images/asset_expanded.png")
+		},
+		function(){
+			$(this).siblings("img").attr("src", "images/asset_collapsed.png")
+	});
+	$(".icon").toggle(
+		function() {
+			$(this).attr("src", "images/asset_expanded.png")
+		},
+		function(){
+			$(this).attr("src", "images/asset_collapsed.png")
+	});
+});
