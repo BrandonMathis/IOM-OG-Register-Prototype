fa0476b7f3e337682e589f6480fd43880ff86922
BrandonMathis <Brandon@KeysetTS.com>
2010-09-10 11:41:44 -0400

    Cleaned up asset index with tables and modified and restored link to asset

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-	$(".asset-name").toggle(
-		function() {
-			$(this).siblings("img").attr("src", "images/asset_expanded.png")
-		},
-		function(){
-			$(this).siblings("img").attr("src", "images/asset_collapsed.png")
-	});
-	$(".icon").toggle(
+	$("#show-assets img.icon").toggle(
 		function() {
 			$(this).attr("src", "images/asset_expanded.png")
 		},
 		function(){
 			$(this).attr("src", "images/asset_collapsed.png")
-	});
+		}
+	);
 });
