23fef79292f56b1a6fe7d0fa1b22eb82887192c7
BrandonMathis <Brandon@KeysetTS.com>
2010-09-22 10:05:04 -0400

    Changed deploy script and version

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -3,7 +3,6 @@ set :user, "deploy"
 
 set :scm, :git
 set :repository, "git@github.com:assetricity/mimosa-active-registry.git"
-set :branch, "ccom3-3"
 
 set :deploy_to, "/home/#{user}/#{application}"
 set :deploy_via, :remote_cache
