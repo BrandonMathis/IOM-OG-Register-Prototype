3f6b2d75cd05d6b8f6ce753df9e050be602d5d82
BrandonMathis <Brandon@KeysetTS.com>
2010-10-28 14:10:04 -0400

    Added test for delete ccom entity and test client title

--- test/functional/ccom_rest_controller_test.rb
+++ test/functional/ccom_rest_controller_test.rb
@@ -1,19 +1,34 @@
 require 'test_helper'
 
 class CcomRestControllerTest < ActionController::TestCase
-  context "rest controller" do
+  context "Deleting a CCOMEntity via RESTful call" do
     setup do
       @entity = Factory.create(:ccom_entity)
     end
-    should "Delete the CCOM Entity and return the deleted entities XML" do
+    
+    should 'delete the entity' do
+      assert_difference('CcomEntity.count', -1) do
+        delete :destroy, :id => @entity.g_u_i_d, :format => 'xml'
+      end
+    end
+    
+    should "return xml of destroyed entity" do
       delete :destroy, :id => @entity.guid, :format => 'xml'
       @doc = Nokogiri::XML.parse(@response.body)
       guid = @doc.mimosa_xpath("/CCOMData/Entity/GUID").first.content
       assert guid =~ /(^(\{{0,1}([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}\}{0,1})$|^$)/
     end
-    should "deleted the entity" do
-      assert_difference('CcomEntity.count', -1) do
+    
+    context "With a GUID that doesnt exsist in db" do
+      setup do
         delete :destroy, :id => @entity.g_u_i_d, :format => 'xml'
+      end
+      
+      should 'give an error when GUID doesnt exsist' do
+        delete :destroy, :id => @entity.g_u_i_d, :format => 'xml'
+        @doc = Nokogiri::XML.parse(@response.body)
+        assert_equal "Could not find requested CCOM Entity with given GUID", @doc.xpath("/CCOMError/errorMessage").first.content
+        assert_equal "deleteEntity", @doc.xpath("/CCOMError/method").first.content
       end
     end
   end
