412be518e39b8c73bf25a8964b8633e87b67dc02
BrandonMathis <Brandon@KeysetTS.com>
2010-11-22 13:06:26 -0500

    All instances of database set to working_db for a user are cleared when database is deleted

--- lib/active_registry.rb
+++ lib/active_registry.rb
@@ -1,6 +1,7 @@
 module ActiveRegistry
   def self.find_database(user_id)
-    if user = User.find_by_id(user_id)
+    user = User.find_by_id(user_id)
+    if user && user.working_db
       database = user.working_db.name + "_dev"  if RAILS_ENV == 'development'
       database = user.working_db.name + "_prod" if RAILS_ENV == 'production'
       database = user.working_db.name + "_test" if RAILS_ENV == 'test'
