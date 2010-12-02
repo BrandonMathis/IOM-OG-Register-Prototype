11473ecfddbc7fe90f53e73e6800601c4a45b790
BrandonMathis <Brandon@KeysetTS.com>
2010-12-02 17:36:43 -0500

    Added more documentation

--- app/models/segment_config_network.rb
+++ app/models/segment_config_network.rb
@@ -1,11 +1,13 @@
 class SegmentConfigNetwork < CcomObject
   has_one :network, :class_name => "Network", :xml_element => "Network"
   
+  # Will destroy the SegmentConfigNetwork and all related entities
   def destroy
     super
     Network.find_by_guid(network.guid).destroy if network && Network.find_by_guid(network.guid)
   end
   
+  # Will duplicated the SegmentConfigNetwork and all related entities
   def dup_entity(options = {})
     entity = super(entity)
     entity.network = self.network.dup_entity(options) if network
