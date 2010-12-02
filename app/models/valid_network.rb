465067835013d68070853f99d4f037eaf8a5fa70
KeysetTS <Brandon@KeysetTS.com>
2010-12-02 18:55:22 -0500

    Finished documentation for models

--- app/models/valid_network.rb
+++ app/models/valid_network.rb
@@ -1,16 +1,19 @@
 class ValidNetwork < CcomObject
   has_one :network, :class_name => "Network", :xml_element => "Network"
     
+  # Will destroy the ValidNetwork and all associated entities
   def destroy
     Network.find_by_guid(network.guid).destroy if network && Network.find_by_guid(network.guid)
     super
   end
   
+  # XML builder for the ValidNetwork (called from to_xml)
   def build_xml(builder)
     super
     builder.Network{|b| network.build_xml(b) } if network
   end
   
+  # Will duplicated the ValidNetwork and all assocated entities
   def dup_entity(options = {})
     entity = super(options)
     entity.network = self.network.dup_entity(options) if network
